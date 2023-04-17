42f9d04d778588169f5785857de660f1a562fdc5
michael-megan <sba21652@student.cct.ie>
2023-04-17 21:50:22 +0100

    Add separate function to process a line.

--- run.py
+++ run.py
@@ -2,12 +2,19 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
+def process_line(line):
+    return line.upper()
 
 # Basic loop for the program
 while True:
     line = input("] ")
     if line.upper() == "BYE":
         break
-    print(line)
-    
+    # Process line
+    print(process_line(line))
+
+
+
 print("Goodbye!")
+
+
