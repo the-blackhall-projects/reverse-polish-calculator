fe7bba5076994e9ee511f417a27c92a13160e9c3
michael-megan <sba21652@student.cct.ie>
2023-04-24 19:47:41 +0100

    Split input line into tokens

--- run.py
+++ run.py
@@ -2,10 +2,16 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
+import os;
+
 def process_line(line):
-    return line.upper()
+    line = line.upper();
+
+    tokens = line.split();
+    return tokens
 
 # Basic loop for the program
+
 while True:
     line = input("] ")
     if line.upper() == "BYE":
