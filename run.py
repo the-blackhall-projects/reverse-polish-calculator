50021ae49dbccfcb3b50287ddc995af4515e8543
michael-megan <sba21652@student.cct.ie>
2023-04-25 22:50:57 +0100

    Added stack length checking for functions

--- run.py
+++ run.py
@@ -65,8 +65,6 @@ def is_function(str):
     return str in {"SIN", "COS", "TAN"}
 
 
-
-
     
 def process_line(stack, line):
 
@@ -84,10 +82,10 @@ def process_line(stack, line):
                 stack.push(token)
             elif is_operator(token):
                 if stack.length() < 2:
-                    print("Stack to small for operation - minimum two elements required for", token)
+                    print("Stack to small for operation - minimum two operands required for", token)
                     return stack
                 elif not is_num(stack.peek(1)) or not is_num(stack.peek(0)):
-                    print("Two top-most elements in stack must be numeric for operation")
+                    print("Two top-most elements in stack must be numeric for operation", token)
                     return stack
                 
                 num2 = float(stack.pop())
@@ -101,6 +99,13 @@ def process_line(stack, line):
                 elif token == '/':
                     stack.push(str(num1 / num2))
             elif is_function(token):
+                if stack.length() < 1:
+                    print("Stack to small for operation - minimum one parameter required for", token)
+                    return stack
+                elif not is_num(stack.peek(0)):
+                    print("Top-most element in stack must be numeric for", token)
+                    return stack
+
                 num = float(stack.pop())
                 if token == "SIN":
                     stack.push(str(math.sin(num)))
